f3e8d8392e94cc233ab08fa614d7919a79b6656b
Aimerick-Noua <aimericknoua4@gmail.com>
2024-12-06 21:48:29 +0100

    initial commit

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,36 @@
+import { Component, inject, input, Input, signal } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+
+  // @Input({required:true}) results?:{
+  //   year: number;
+  //   interest: number;
+  //   valueEndOfYear: number;
+  //   annualInvestment: number;
+  //   totalInterest: number;
+  //   totalAmountInvested: number ;
+  // }[]
+  // results = input<{
+  //   year: number;
+  //   interest: number;
+  //   valueEndOfYear: number;
+  //   annualInvestment: number;
+  //   totalInterest: number;
+  //   totalAmountInvested: number ;
+  // }[] | undefined>(undefined)
+
+  private investmentService = inject(InvestmentService);
+
+  get results(){
+    return this.investmentService.resultsData;
+  }
+}
