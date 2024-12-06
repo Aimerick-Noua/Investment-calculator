1968cac8124ecbb1311856d5cc2fb730837518db
Aimerick-Noua <aimericknoua4@gmail.com>
2024-12-06 21:56:12 +0100

    using signals in services

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input, Input, signal } from '@angular/core';
+import { Component, computed, inject, input, Input, signal } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
 import { InvestmentService } from '../investment.service';
 
@@ -11,26 +11,8 @@ import { InvestmentService } from '../investment.service';
 })
 export class InvestmentResultsComponent {
 
-  // @Input({required:true}) results?:{
-  //   year: number;
-  //   interest: number;
-  //   valueEndOfYear: number;
-  //   annualInvestment: number;
-  //   totalInterest: number;
-  //   totalAmountInvested: number ;
-  // }[]
-  // results = input<{
-  //   year: number;
-  //   interest: number;
-  //   valueEndOfYear: number;
-  //   annualInvestment: number;
-  //   totalInterest: number;
-  //   totalAmountInvested: number ;
-  // }[] | undefined>(undefined)
-
   private investmentService = inject(InvestmentService);
-
-  get results(){
-    return this.investmentService.resultsData;
-  }
+  results = this.investmentService.resultsData.asReadonly();
+  // results = computed(()=> this.investmentService.resultsData);
+  
 }
